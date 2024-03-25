f8d7edb02cdf284d3a037a7f0c8b59aaa6ad1550
Talha <talha7en@gmail.com>
2024-03-25 03:55:03 -0400

    Some UI imporvements

--- src/ui.py
+++ src/ui.py
@@ -14,9 +14,9 @@ def selectfile():
     global image_file
     image_file=filename
     img=Image.open(filename)
-    image=CTkImage(light_image=img,dark_image=img,size=(300,400))
+    image=CTkImage(light_image=img,dark_image=img,size=(400,400))
     imLabel=CTkLabel(app,text="",image=image)
-    imLabel.place(relx = 0.3, rely = 0.3,anchor="center")
+    imLabel.place(relx = 0.5, rely = 0.5,anchor="center")
 
 
 
@@ -26,19 +26,20 @@ def classify():
     print(image_file)
     prediction_text=predict_image(model, image_file, device='cpu')
 
-    frame=CTkFrame(master=app,fg_color="green",border_color="black",border_width=2)
-    frame.place(relx = 0.8, rely = 0.1,anchor="center")
-    txt=CTkLabel(master=frame,text=prediction_text)
+    frame=CTkFrame(master=app,fg_color="transparent",border_color="white",border_width=2)
+    frame.place(relx = 0.5, rely = 0.1,anchor="center")
+    txt=CTkLabel(master=frame,text=prediction_text,font=("Roboto",40),pady=5,padx=5)
     txt.pack(anchor="s",expand=True,pady=3,padx=3)
 
 
 
 button_to_select = CTkButton(master=app, text = "Choose file", fg_color = "blue", command = selectfile)
 button_to_select.pack(padx = 5, pady = 5)
-button_to_select.place(relx = 0.5, rely = 0.9,anchor="center")
+button_to_select.place(relx = 0.4, rely = 0.9,anchor="center")
 
 classify_button=CTkButton(master=app,text="Classify",fg_color="green",command=classify)
-classify_button.place(relx=0.7,rely=0.7,anchor="center")
+classify_button.place(relx=0.6,rely=0.9,anchor="center")
 
 app.mainloop()
+
 
